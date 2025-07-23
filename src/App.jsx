cae40361b8380607c487d021afa1d8611c2fa302
Egloris <egloris@icloud.com>
2025-07-23 15:33:42 +0100

    Fixes

--- src/App.jsx
+++ src/App.jsx
@@ -75,7 +75,7 @@ const App = () => {
           <h2 className='mt-[40px]'>All Movies</h2>
 
           {isLoading ? (
-            <Spinner />
+             <p className='text-white'>Loading...</p>
           ) : errorMessage ? (
             <p className='text-red-500'>{errorMessage}</p>
           ) : (
