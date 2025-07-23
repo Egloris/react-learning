5e3e3d9384ad59d0a52fb94812ff6ec27c8ccd4e
Egloris <egloris@icloud.com>
2025-07-23 10:00:52 +0100

    39

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 const Card = ({ title }) => {
+  const [count, setCount] = useState(0);
+  const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(() => {
+    console.log(`${title} currently ${hasLiked ? "unliked" : "liked"}!`);
+  })
+
   return (
     <div className='card'>
       <h2>{title}</h2>
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? "Unlike 💔" : "Like ❤️"}
+      </button>
     </div>
   )
 }
 
 const App = () => {
+
   return (
     <div className='card-container'>
       <Card title={"Star Wars"} />
